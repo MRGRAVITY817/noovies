422dd888487e388faa1d8d277fe590ee8faa46dd
SeongHoon Wee <mrgravity817@gmail.com>
2021-12-02 10:44:49 +0900

    Process multi assets preloading

--- App.js
+++ App.js
@@ -5,13 +5,26 @@ import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./startup.jpg"));
-    // await Image.prefetch("") <- prefetch remote image
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./startup.jpg"),
+      "https://images.unsplash.com/photo-1638308677353-c00cf6436741?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
