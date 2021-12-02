dc59839ae6d9703e26c6a1c8041de9cf4c90b3e9
SeongHoon Wee <mrgravity817@gmail.com>
2021-12-02 10:48:57 +0900

    Better preloading with useAssets and useFonts hooks

--- App.js
+++ App.js
@@ -3,37 +3,16 @@ import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
-
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require("./startup.jpg"),
-      "https://images.unsplash.com/photo-1638308677353-c00cf6436741?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    ]);
-    await Promise.all([...fonts, ...images]);
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.log}
-      />
-    );
+  const [assets] = useAssets([require("./startup.jpg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  // When you use hooks, it gives simplicity,
+  // but if you want to give more functionalities,
+  // you should make own preloading function.
+  if (!assets || !loaded) {
+    return <AppLoading onError={console.log} />;
   }
   return (
     <View style={styles.container}>
